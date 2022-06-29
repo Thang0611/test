30b5e554e951bca461c0735415a4005debdc65ea
Thang Nguyen <nguyenhuuthanga3@gmail.com>
2022-06-29 16:33:16 +0700

    first commit

---
+++ demo1.js
@@ -0,0 +1,17 @@
+						
+//serve-mp3.js
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+    res.writeHead(200,{"Content-Type":"video/mp4"})
+	fs.exists('video.mp4',function(exist){
+        if (exist){
+            var rstream= fs.createReadStream('video.mp4')
+            rstream.pipe(res)
+        }
+        else {
+            res.send("ERROR")
+            res.end()
+        }
+    })
+}).listen(3000);
